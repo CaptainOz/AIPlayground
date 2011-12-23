92c122604f3d220f80c96b0976beb3dcabfa7fbe
Oz Wolfe <dreadpirate@lifewanted.com>
2011-12-23 19:06:29 +0000

    Massive documentation update

--- static/genetic/1/app.js
+++ static/genetic/1/app.js
@@ -1,19 +1,30 @@
 var maze = (function(){
+    // The maze is just an array of integers.
+    //  1 - A wall.
+    //  0 - An open area.
+    //  S - The start.
+    //  E - The end
+    var S = 2;
+    var E = 3;
     var maze = [
         [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ],
         [ 1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1 ],
-        [ 8, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1 ],
+        [ E, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1 ],
         [ 1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1 ],
         [ 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 1 ],
         [ 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 1 ],
         [ 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 1 ],
-        [ 1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 5 ],
+        [ 1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, S ],
         [ 1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1 ],
         [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ]
     ];
-    var self    = {};
-    self.width  = maze[0].length;
-    self.height = maze.length;
+    
+    // Calculate a few constant values used for rendering.
+    var self    = {
+        width  : maze[0].length,
+        height : maze.length,
+        tile   : [ 'B', 'W', 'S', 'E' ]
+    };
     var pixelWidth   = Math.floor( g_canvas.width  / self.width  );
     var pixelHeight  = Math.floor( g_canvas.height / self.height );
     var pixelPadding = 0;
@@ -25,10 +36,10 @@ var maze = (function(){
         var row = maze[y];
         for( var x in row ){
             var cell = row[x];
-            if( cell == 5 ){
+            if( cell == S ){
                 start = [ x, y ];
             }
-            if( cell == 8 ){
+            if( cell == E ){
                 end = [ x, y ];
             }
             if( start && end ){
@@ -42,22 +53,32 @@ var maze = (function(){
     self.start = start;
     self.end   = end;
 
+    /// Draws the maze onto the global canvas surface.
     self.drawMaze = function(){
+        // All tiles are drawn filled in, just the color is different.
         var style = {
             type : 'fill'
         };
+        
+        // Loop through the whole maze construct.
         for( var y in maze ){
             var row = maze[y];
             var vertOffset  = y * pixelHeight + (y+1) * pixelPadding;
             for( var x in row ){
                 var cell = row[x];
                 var horzOffset = x * pixelWidth + (x+1) * pixelPadding;
+
+                // A wall
                 if( cell == 1 ){
                     style.style = 'black';
                 }
+                
+                // A blank square
                 else if( cell == 0 ){
                     style.style = 'white';
                 }
+                
+                // Either the exit or enterance.
                 else {
                     style.style = 'red';
                 }
@@ -65,21 +86,43 @@ var maze = (function(){
             }
         }
     };
+    
+    /// Fetches the tile type for the specified coordinates.
+    self.getTile = function( x, y ){
+        return self.tile[ maze[y][x] ];
+    };
 
     return self;
 })();
 
+/// A genome.
+///
+/// Each chromosome in this genome is either a 1 or a 0. Each gene is made of
+/// two chromosomes which give 4 possible combinations. They are read as follows:
+///  - 00 : North
+///  - 01 : East
+///  - 10 : South
+///  - 11 : West
 var Genome = (function(){
-    function Genome(){
+    /// Constructor.
+    ///
+    /// This has one parameter which can be either a Genome to copy or an
+    /// integer indicating the length of a random Genome to construct. If no
+    /// parameter is proved an empty Genome is constructed.
+    ///
+    /// @param {Genome} arg A Genome to clone.
+    /// @param {Number} arg The length of a Genome to randomly generate.
+    function Genome( arg ){
         this._genes    = [];
         this._geneSize = 2;
         this._fitness  = 0;
         this._position = 0;
         
-        if( arguments.length ){
-            var arg = arguments[0];
+        // If we have an argument, why kind is it?
+        if( arg ){
             if( arg instanceof Genome ){
-                this._genes = arg._genes.concat([]);
+                this._genes    = arg._genes.concat([]);
+                this._geneSize = arg._geneSize;
             }
             else if( $.isNumeric( arg ) ){
                 _populateRandom.call( this, arg );
@@ -87,21 +130,50 @@ var Genome = (function(){
         }
     }
     
+    /// Generates a random chromosome string.
+    ///
+    /// @this {Genome}
+    ///
+    /// @param {Number} length The number of genes to generate.
     function _populateRandom( length ){
         for( var i = 0; i < length * this._geneSize; ++i ){
             this._genes.push( Math.floor( Math.random * 2 ) );
         }
     }
     
+    /// Randomly mutates this genome.
+    ///
+    /// The mutation rate should be between 0 and 1. A mutation rate of 1 would
+    /// indicate that every gene be mutated.
+    ///
+    /// Mutation in this case simply means to toggle a single chromosome.
+    ///
+    /// @this {Genome}
+    ///
+    /// @param {Number} mutationRate The rate at which to mutate the genome.
     function _mutate( mutationRate ){
         for( var i in this._genes ){
             if( Math.random() > mutationRate ){
                 continue;
             }
-            this._genes[i] = this._genes[i] ? 0 : 1;
+            this._genes[i] ^= 1;
         }
     }
     
+    /// Combines this Genome with the provided dad Genome.
+    ///
+    /// The cross over rate should be between 0 and 1. A 1 would guarantee that
+    /// the genes get crossed. When crosover doesn't happen this Genome and the
+    /// dad are cloned.
+    ///
+    /// This crossover method is a single point swap.
+    ///
+    /// @this {Genome}
+    ///
+    /// @param {Genome} dad           The genome to mix with.
+    /// @param {Number} crossOverRate The chance of mixing instead of cloning.
+    ///
+    /// @return {Array<Genome>} The babies generated from the crossover.
     function _crossOver( dad, crossOverRate ){
         // Are we going to cross over at all, or just clone?
         if( Math.random() > crossOverRate || this === dad ){
@@ -112,12 +184,12 @@ var Genome = (function(){
         }
         
         // We're crossing over. Mix those genes, but make sure we only mix whole
-        // chromosones.
+        // genes.
         var litter = [ new Genome(), new Genome() ];
         var crossPoint = Math.floor( Math.random() * this._genes.length );
-        crossPoint = crossPoint - (crossPoint % this._geneSize);
+        crossPoint -= (crossPoint % this._geneSize);
         
-        // Copy the chromosones over, crossing over at the point decided above.
+        // Copy the genes over, crossing over at the point decided above.
         var push = Array.prototype.push;
         push.apply( litter[0]._genes, this._genes.slice( 0, crossPoint ) );
         push.apply( litter[1]._genes,  dad._genes.slice( 0, crossPoint ) );
@@ -128,6 +200,13 @@ var Genome = (function(){
         return litter;
     }
     
+    /// Breed this Genome with another to generate offspring.
+    ///
+    /// @param {Genome} dad
+    /// @param {Number} mutationRate
+    /// @param {Number} crossOverRate
+    ///
+    /// @return {Array<Genome>}
     Genome.prototype.mate = function( dad, mutationRate, crossOverRate ){
         var litter = _crossOver.call( this, dad, crossOverRate );
         for( var i in litter ){
